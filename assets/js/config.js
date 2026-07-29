12f2c966e8bb0ac35b377222b3815b66e9fd0ccb
MarionCarrillo00 <marion.carrillo@hotmail.fr>
2026-07-29 20:28:52 +0200

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -3,3 +3,5 @@ const SUPABASE_URL = "https://rlykropbkwozttbwlpey.supabase.co";
 
 const SUPABASE_PUBLISHABLE_KEY =
   "sb_publishable_Jy-aJ5MrCIyeCIwnDt6SXw_OKp_Cu7Z";
+
+const TMDB_DISCOVER_URL = ${SUPABASE_URL}/functions/v1/tmdb-discover;
